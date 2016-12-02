b0e9baefe1f3e005224584eab3ca428b5fb4a480
Otto G <ottodevs@gmail.com>
2016-12-02 13:23:18 +0100

    Initial commit

---
+++ src/js/BerlinClock.js
@@ -0,0 +1,26 @@
+var BerlinClock = {
+    tick: function(){
+        var time = new Date(),
+            second = time.getSeconds();
+            minute = time.getMinutes();
+            hour = time.getHours();
+    },
+    updateSeconds: function(second){
+
+    },
+    updateHours: function(hour){
+
+    },
+
+    updateMinutes: function(minute){
+
+    },
+
+    updateClock: function(hour, minute, second){
+
+    },
+
+    reset: function(){
+
+    }
+}
