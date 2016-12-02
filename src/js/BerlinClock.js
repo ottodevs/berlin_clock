a6e4d299cd928373a5e598441932dc3dade0c3a3
Otto G <ottodevs@gmail.com>
2016-12-02 13:29:23 +0100

    initial refactor to conform html standards

--- src/js/BerlinClock.js
+++ src/js/BerlinClock.js
@@ -1,26 +1,32 @@
 var BerlinClock = {
-    tick: function(){
-        var time = new Date(),
-            second = time.getSeconds();
-            minute = time.getMinutes();
-            hour = time.getHours();
-    },
-    updateSeconds: function(second){
+  tick: function () {
+    var time = new Date(),
+      second = time.getSeconds()
+    minute = time.getMinutes()
+    hour = time.getHours()
+  },
+  updateSeconds: function (second) {
 
-    },
-    updateHours: function(hour){
+  },
+  updateHours: function (hour) {
 
-    },
+  },
 
-    updateMinutes: function(minute){
+  updateMinutes: function (minute) {
 
-    },
+  },
 
-    updateClock: function(hour, minute, second){
+  updateClock: function (hour, minute, second) {
 
-    },
+  },
 
-    reset: function(){
+  reset: function () {
 
-    }
+  }
 }
+
+$(function () {
+  BerlinClock.tick()
+  setInterval(BerlinClock.tick.bind(BerlinClock), 1000)
+}()
+)
